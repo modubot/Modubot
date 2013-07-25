5c85c991d5592c9b7edc5313dac7de466c71ab6d
clone1018 <luke@axxim.net>
2013-07-25 11:43:50 -0500

    Adding in some more framework

--- bot.php
+++ bot.php
@@ -2,6 +2,15 @@
 
 require __DIR__ . '/bootstrap/autoload.php';
 
+use Modubot\Modubot\Bot;
+use Modubot\Modubot\BotConfig;
+
+// Load the configuration
+$config = new BotConfig('bot/config/bots.json');
 
 // Start the bot
 $bot = require_once __DIR__ . '/bootstrap/start.php';
+
+$bot = new Bot($config);
+
+$bot->start();
---
+++ src/Modubot/Modubot/Bot.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modubot\Modubot;
+
+
+class Bot {
+
+    public function __construct(BotConfig $config) {
+
+    }
+
+    /**
+     * Let's register what we need to for the bot to work.
+     */
+    private function registerLibraries() {
+
+    }
+
+    public function start() {
+
+        $this->registerLibraries();
+
+    }
+
+}
